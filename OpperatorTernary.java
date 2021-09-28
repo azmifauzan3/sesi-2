6bee8cd4d12d4ed65db791374a963daa6dd9f431
azmifauzan3 <81280706+azmifauzan3@users.noreply.github.com>
2021-09-28 19:22:47 +0700

    Add files via upload

---
+++ OpperatorTernary.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Muhammad Azmi Fauzan
+ */
+public class OpperatorTernary {
+    public static void main(String[] args){
+        int HargaBuku = 55000;
+        int UangDibawa = 50000;
+        String result = HargaBuku < UangDibawa ? "Membeli":"Tidak Membeli";
+        System.out.println(result);
+    }
+    
+}
